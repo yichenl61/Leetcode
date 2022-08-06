bb87c103ff663dd89500d59a419db634f256d7a9
yichenl61 <40710142+yichenl61@users.noreply.github.com>
2022-08-05 21:15:48 -0400

    Time: 17 ms (34.73%), Space: 11.2 MB (60.29%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,7 +17,7 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        map<Node*, Node*> map;
+        unordered_map<Node*, Node*> map;
         
         Node* curr = head;
         while(curr != NULL)
